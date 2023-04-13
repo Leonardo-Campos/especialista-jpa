70c856241678594f736e2d2535292ddb2a8a7642
Leonardo-Campos <leonardo.campos.p.lopes@gmail.com>
2023-04-13 11:16:19 -0300

    Mapeamento relacionamentos um-para-um com @OneToOne

--- src/main/java/com/algaworks/ecommerce/model/Pedido.java
+++ src/main/java/com/algaworks/ecommerce/model/Pedido.java
@@ -34,13 +34,16 @@ public class Pedido {
     @Column(name = "data_conclusao")
     private LocalDateTime dataConclusao;
 
-    @Column(name = "nota_fiscal_id")
-    private Integer notaFiscalId;
+    @OneToOne(mappedBy = "pedido")
+    private NotaFiscal notaFiscal;
 
     private BigDecimal total;
 
     @Enumerated(EnumType.STRING)
     private StatusPedido status;
+
+    @OneToOne(mappedBy = "pedido")
+    private PagamentoCartao pagamento;
 
     @Embedded
     private EnderecoEntregaPedido enderecoEntrega;
