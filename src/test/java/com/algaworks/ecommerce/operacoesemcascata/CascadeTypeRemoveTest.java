edd61e8af54e7485448dc6297fa9fee72c0b78c2
Leonardo-Campos <leonardo.campos.p.lopes@gmail.com>
2023-05-02 08:08:33 -0300

    Removendo objetos órfãos com a propriedade orphanRemoval

--- src/test/java/com/algaworks/ecommerce/operacoesemcascata/CascadeTypeRemoveTest.java
+++ src/test/java/com/algaworks/ecommerce/operacoesemcascata/CascadeTypeRemoveTest.java
@@ -1,13 +1,43 @@
 package com.algaworks.ecommerce.operacoesemcascata;
 
 import com.algaworks.ecommerce.EntityManagerTest;
-import com.algaworks.ecommerce.model.ItemPedido;
-import com.algaworks.ecommerce.model.ItemPedidoId;
-import com.algaworks.ecommerce.model.Pedido;
+import com.algaworks.ecommerce.model.*;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class CascadeTypeRemoveTest extends EntityManagerTest {
+
+    // @Test
+    public void removerItensOrfaos() {
+        Pedido pedido = entityManager.find(Pedido.class, 1);
+
+        Assert.assertFalse(pedido.getItens().isEmpty());
+
+        entityManager.getTransaction().begin();
+        pedido.getItens().clear();
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Pedido pedidoVerificacao = entityManager.find(Pedido.class, pedido.getId());
+        Assert.assertTrue(pedidoVerificacao.getItens().isEmpty());
+    }
+
+    @Test
+    public void removerRelacaoProdutoCategoria() {
+        Produto produto = entityManager.find(Produto.class, 1);
+
+        Assert.assertFalse(produto.getCategorias().isEmpty());
+
+        entityManager.getTransaction().begin();
+        produto.getCategorias().clear();
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+        Assert.assertTrue(produtoVerificacao.getCategorias().isEmpty());
+    }
 
     // @Test
     public void removerPedidoEItens() {
