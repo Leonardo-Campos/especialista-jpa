b7c13de51ecdf46bb67581ac3f6a4ddc299aece2
Leonardo-Campos <leonardo.campos.p.lopes@gmail.com>
2023-05-10 08:48:09 -0300

    Usando funções para números

--- src/test/java/com/algaworks/ecommerce/jpql/FuncoesTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/FuncoesTest.java
@@ -1,13 +1,30 @@
 package com.algaworks.ecommerce.jpql;
 
 import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Categoria;
 import org.junit.Assert;
 import org.junit.Test;
 
 import javax.persistence.TypedQuery;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.TimeZone;
 
 public class FuncoesTest extends EntityManagerTest {
+
+    @Test
+    public void aplicarFuncaoNumero() {
+        String jpql = "select abs(p.total), mod(p.id, 2), sqrt(p.total) from Pedido p " +
+                " where abs(p.total) > 1000";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(arr[0] + " | " + arr[1] + " | " + arr[2]));
+    }
 
     @Test
     public void aplicarFuncaoData() {
