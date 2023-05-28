9d1cd815a7806ab8ebcc39d77d2cbc15c951f910
Leonardo-Campos <leonardo.campos.p.lopes@gmail.com>
2023-05-28 12:16:59 -0300

    Agrupando registros com funções no group by

--- src/test/java/com/algaworks/ecommerce/criteria/PathExpressionsTest.java
+++ src/test/java/com/algaworks/ecommerce/criteria/PathExpressionsTest.java
@@ -14,6 +14,8 @@ import java.util.List;
 
 public class PathExpressionsTest extends EntityManagerTest {
 
+
+
     @Test
     public void buscarPedidosComProdutoDeIDIgual1Exercicio() {
         CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
